efa4a229fa7a7caa6ddddbb0fb4fa4922d635ca4
SaiTp <37739724+SaiTp@users.noreply.github.com>
2021-03-28 22:17:49 +0530

    Update PrimeSum.cpp

--- Math/PrimeSum.cpp
+++ Math/PrimeSum.cpp
@@ -2,19 +2,12 @@
 
 vector<int> Solution::primesum(int A) {
     
-    vector<bool> val;
+    vector<bool> val(A,true);
     vector<int> sol;
-    // unordered_map<int, bool> map;
-    
-    int i;
-    
-    for(i = 0; i < A; i++){
-        val.push_back(true);
-    }
-    
+    int i,j;
     for(i = 2; i <= sqrt(A); i++){
         if(val[i] == true){
-            for(int j = 2; i*j <= A; j++){
+            for( j = 2; i*j <= A; j++){
                 val[i*j] = false;
             }
         }
@@ -30,30 +23,3 @@ vector<int> Solution::primesum(int A) {
         }
     }
     
-    // for(int i = 2; i < A; i++){
-    //     // map.insert(i, true);
-    //     map[i] = true;
-    // } 
-    
-    // for(int i = 2; i <= sqrt(A); i++){
-    //     if(map.at(i) == true){
-    //         for(int j = 2; i*j <= A; j++){
-    //             map[i*j] = false;
-    //         }
-    //     }
-    // }
-    
-    // for(int i = 2; i < A; i++){
-    //     if(map.at(i) == true){
-    //         int temp = A - i;
-    //         if(map.at(temp) == true){
-    //             sol.push_back(i);
-    //             sol.push_back(temp);
-    //             return sol;
-    //         }
-    //     }
-    // }
-    
-   return sol;
-
-}
